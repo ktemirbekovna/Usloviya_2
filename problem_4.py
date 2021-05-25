0d86556f78cf9359c1d486570ff2d7caa77d9908
ktemirbekovna <tktemirbekovna@gmail.com>
2021-05-25 23:19:25 +0600

    if.elif.else

---
+++ problem_4.py
@@ -0,0 +1,12 @@
+a = 10
+if a == True:
+    print("Всегда true")
+else:
+    print("В любом случае cработает")
+#Problem5
+a = 10//5
+b = 10/5
+if a == b:
+    print((10//5)+(10/5))
+else:
+    print("Они не равны")
